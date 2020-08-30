e7f52a83ae360b295b2eea02b37ddaeba0925341
Asher <asher_github@163.com>
2020-08-30 21:32:59 +0800

    对T003中的程序进行更新 解决过拟合问题 使用更加好的方法构建神经网络

--- venv/src/T003/bike_main.py
+++ venv/src/T003/bike_main.py
@@ -9,69 +9,109 @@ import matplotlib.pyplot as plt
 
 data_path = './dataset/hour.csv'
 rides = pd.read_csv(data_path)
-rides.head()
-counts = rides['cnt'][:50]
-x = np.arange(len(counts))
-y = np.array(counts)
-plt.figure(figsize=(10, 7))
-plt.plot(x, y, 'o-')
-plt.xlabel('X')
-plt.ylabel('Y')
 
-x = Variable(torch.FloatTensor(np.arange(len(counts), dtype= float)))
+# 类别数据处理(One-Hot)
 
-y = Variable(torch.FloatTensor(np.array(counts, dtype=float)))
+dummy_fields = ['season', 'weathersit', 'mnth', 'hr', 'weekday']
+for each in dummy_fields:
+    dummies = pd.get_dummies(rides[each], prefix=each, drop_first=False)
+    rides = pd.concat([rides, dummies], axis=1)
+fields_to_drop =['instant','dteday','season','weathersit','weekday','atemp','mnth','workingday','hr']
+data = rides.drop(fields_to_drop, axis=1)
 
-sz = 10
+# 处理数值变量(归一化处理)
 
-weights = Variable(torch.randn(1, sz),requires_grad = True)
+quant_features = ['cnt', 'temp', 'hum', 'windspeed']
+scaled_features = {}
+for each in quant_features:
+    mean, std = data[each].mean(), data[each].std()
+    scaled_features[each] = [mean, std]
+    data.loc[:, each] = (data[each] - mean)/std
 
-biases = Variable(torch.randn(sz),requires_grad = True)
+# 处理完之后数据集包含17379条记录，59个变量
 
-weights2 = Variable(torch.randn(sz, 1),requires_grad = True)
+# 数据集划分
 
-learning_rate = 0.0001
+test_data = data[-21*24:]
+train_data = data[:-21*24]
 
+target_fields = ['cnt', 'casual', 'registered']
+
+features, targets = train_data.drop(target_fields, axis=1), train_data[target_fields]
+
+test_featrues, test_targets = test_data.drop(target_fields, axis=1), test_data[target_fields]
+
+X = features.values
+Y = targets['cnt'].values
+Y = Y.astype(float)
+
+Y = np.reshape(Y, [len(Y), 1])
 losses = []
 
-for i in range(100000):
-    hidden = x.expand(sz, len(x)).t() * weights.expand(len(x), sz) + biases.expand(len(x), sz)
+input_size = features.shape[1]
+hidden_size = 10
+output_size = 1
+batch_size = 128
+neu = torch.nn.Sequential(
+    torch.nn.Linear(input_size, hidden_size),
+    torch.nn.Sigmoid(),
+    torch.nn.Linear(hidden_size, output_size)
+)
 
-    hidden = torch.sigmoid(hidden)
+cost = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(neu.parameters(), lr=0.01)
 
-    predictions = hidden.mm(weights2)
-
-    loss = torch.mean((predictions - y) ** 2)
-
-    losses.append(loss.data.numpy())
-
-    if i % 10000 == 0:
-        print('loss:', loss)
-
-    loss.backward()
-
-    weights.data.add_(- learning_rate * weights.grad.data)
-
-    biases.data.add_(- learning_rate * biases.grad.data)
-
-    weights2.data.add_(- learning_rate * weights2.grad.data)
-
-    weights.grad.data.zero_()
-
-    biases.grad.data.zero_()
-
-    weights2.grad.data.zero_()
-
-plt.plot(losses)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
+# 模型的训练
+for i in range(1000):
+    batch_loss = []
+    for start in range(0, len(X), batch_size):
+        end = start + batch_size if start + batch_size < len(X) else len(X)
+        xx = Variable(torch.FloatTensor(X[start:end]))
+        yy = Variable(torch.FloatTensor(Y[start:end]))
+        predict = neu(xx)
+        loss = cost(predict, yy)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        batch_loss.append(loss.data.numpy())
+    if i % 100 == 0:
+        losses.append(np.mean(batch_loss))
+        print(i, np.mean(batch_loss))
 
 
-x_data = x.data.numpy()
-plt.figure(figsize=(10, 7))
-xplot, = plt.plot(x_data, y.data.numpy(), 'o')
-yplot, = plt.plot(x_data, predictions.data.numpy())
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend([xplot, yplot],['Data', 'Prediction under 1000000 epochs'])
+
+targets = test_targets['cnt']
+targets = targets.values.reshape([len(targets), 1])
+targets = targets.astype(float)
+
+x = Variable(torch.FloatTensor(test_featrues.values))
+y = Variable(torch.FloatTensor(targets))
+
+
+# 用神经网络进行预测
+
+predict = neu(x)
+
+predict = predict.data.numpy()
+
+# 打印输出损失值
+
+# plt.plot(np.arange(len(losses))*100,losses,'o-')
+# plt.xlabel('epoch')
+# plt.ylabel('MSE')
+# plt.show()
+
+# 测试模型的有效性
+fig, ax = plt.subplots(figsize = (10, 7))
+mean, std = scaled_features['cnt']
+ax.plot(predict * std + mean,label='Prediction')
+ax.plot(targets * std + mean,label='Data')
+ax.legend()
+ax.set_xlabel('Data-time')
+ax.set_ylabel('Counts')
+dates = pd.to_datetime(rides.loc[test_data.index]['dteday'])
+dates = dates.apply(lambda d: d.strftime('%b %d'))
+ax.set_xticks(np.arange(len(dates))[12::24])
+_ = ax.set_xticklabels(dates[12::24], rotation=45)
+
 plt.show()
