d5bde53b1d6e5c4caf800b6221e20c978a58134a
Asher <asher_github@163.com>
2020-09-03 19:57:43 +0800

    对第五章实验进行更新 添加注释

--- venv/src/T005/cnn_main.py
+++ venv/src/T005/cnn_main.py
@@ -77,25 +77,30 @@ muteimg = train_dataset[idx][0].numpy()
 # 因此，我们忽略通道，把图像看作一个灰度矩阵
 # 用imshow画图，会将灰度矩阵自动展现为彩色，不同灰度对应不同的颜色：从黄到紫
 
-plt.imshow(muteimg[0,...])
-print('标签是：',train_dataset[idx][1])
-plt.show()
+# plt.imshow(muteimg[0,...])
+# print('标签是：',train_dataset[idx][1])
+# plt.show()
 
 # 构建网络
 depth = [4, 8]
 
 class ConvNet(nn.Module):
     def __init__(self):
+        # 该函数在创建一个ConvNet对象即调用语句net=ConvNet()时就会被调用
+        # 首先调用父类相应的构造函数
         super(ConvNet, self).__init__()
 
+        # 其次构造ConvNet需要用到的各个神经模块
+        # 注意，定义组件并不是真正搭建组件，只是把基本建筑砖块先找好
+        # 定义一个卷积层，输入通道为1，输出通道为4，窗口大小为5，padding=2
         self.conv1 = nn.Conv2d(1, 4, 5, padding= 2)
-        self.pool = nn.MaxPool2d(2,2)
-
+        self.pool = nn.MaxPool2d(2,2) #定义一个池化层，一个窗口为2X2的池化运算
+        # 第二层卷积，输入通道为depth[0]，输出通道为depth[1]，窗口为5，padding=2
         self.conv2 = nn.Conv2d(depth[0], depth[1], 5, padding= 2)
-
+        # 一个线性连接层，输入尺寸为最后一层立方体的线性平铺，输出层512个节点。
         self.fc1 = nn.Linear(image_size // 4 * image_size // 4 * depth[1], 512)
 
-        self.fc2 = nn.Linear(512, num_classes)
+        self.fc2 = nn.Linear(512, num_classes)#最后一层线性分类单元，输入为512，输出为要做分类的类别数
 
     def forward(self, x):
         x = self.conv1(x)
@@ -129,8 +134,10 @@ class ConvNet(nn.Module):
         return (feature_map1,feature_map2)
 
 # 运行模型
-
-net = ConvNet()
+if torch.cuda.is_available():
+    net = ConvNet().cuda()
+else:
+    net =  ConvNet()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
@@ -138,43 +145,66 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 record = []
 weights = []
 
-for epoch in range(num_epochs):
-
-    train_rights = []
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = Variable(data), Variable(target)
-        net.train()
-        output = net(data)
-        loss = criterion(output, target)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        right = rightness(output, target)
-        train_rights.append(right)
-
-
-        if batch_idx % 100 == 0:
-            net.eval()
-            val_rights = []
-
-            for(data, target) in validation_loader:
-                data, target = Variable(data), Variable(target)
-                output = net(data)
-                right = rightness(output, target)
-                val_rights.append(right)
-
-            train_r = (sum([tup[0] for tup in train_rights]), sum([tup[1] for tup in train_rights]))
-            val_r = (sum([tup[0] for tup in val_rights]), sum([tup[1] for tup in val_rights]))
+# for epoch in range(num_epochs):
+#
+#     train_rights = []
+#
+#     for batch_idx, (data, target) in enumerate(train_loader):
+#         if torch.cuda.is_available():
+#             data, target = Variable(data).cuda(), Variable(target).cuda()
+#         else:
+#             data, target = Variable(data), Variable(target)
+#         net.train()
+#         output = net(data)
+#         loss = criterion(output, target)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         right = rightness(output, target)
+#         train_rights.append(right)
+#
+#
+#         if batch_idx % 100 == 0:
+#             net.eval()
+#             val_rights = []
+#
+#             for(data, target) in validation_loader:
+#                 if torch.cuda.is_available():
+#                     data, target = Variable(data).cuda(), Variable(target).cuda()
+#                 else:
+#                     data, target = Variable(data), Variable(target)
+#                 output = net(data)
+#                 right = rightness(output, target)
+#                 val_rights.append(right)
+#
+#             train_r = (sum([tup[0] for tup in train_rights]), sum([tup[1] for tup in train_rights]))
+#             val_r = (sum([tup[0] for tup in val_rights]), sum([tup[1] for tup in val_rights]))
+#             print('训练周期:{} [{}/{} ({:.0f}%)]\t, loss: {:.6f}\t, 训练正确率: {:.2f}%\t,'
+#                   '校验正确率：{:.2f}%'.format(
+#                 epoch, batch_idx * len(data), len(train_loader.dataset),
+#                 100. * batch_idx / len(train_loader), loss.item(),
+#                 100. * train_r[0] / train_r[1],
+#                 100. * val_r[0] / val_r[1]
+#             ))
+#
+#             record.append((100 - 100. * train_r[0] / train_r[1], 100 - 100. * val_r[0]/val_r[1]))
+#
+#             weights.append([net.conv1.weight.data.clone(), net.conv1.bias.data.clone(),
+#                             net.conv2.weight.data.clone(), net.conv2.bias.data.clone()])
 
 
 # 测试模型
-
+# torch.save(net,'./save_model/cnn_model.mdl')
+net = torch.load('./save_model/cnn_model.mdl')
+# print('保存完毕')
 net.eval()
 vals = []
 
 for data, target in test_loader:
-    data, target = Variable(data, volatile = True), Variable(target)
+    if torch.cuda.is_available():
+        data, target = Variable(data).cuda(), Variable(target).cuda()
+    else:
+        data, target = Variable(data, volatile = True), Variable(target)
     output = net(data)
     val = rightness(output, target)
     vals.append(val)
@@ -190,3 +220,4 @@ plt.figure(figsize=(10, 7))
 plt.plot(record)
 plt.xlabel('Steps')
 plt.ylabel('Error rate')
+plt.show()
